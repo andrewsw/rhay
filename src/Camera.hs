f30afbaf82ed661dac95ef0c946b08b121025c72
Andrew Sackville-West <andrew@swclan.homelinux.org>
2013-12-28 12:12:13 -0800

    minor refactor of camera ray construction, fix aspect ratio
    * we have to adjust the aspect ratio of one or the other coordinate values
     in all screen pixels to ensure images aren't squashed

--- src/Camera.hs
+++ src/Camera.hs
@@ -14,14 +14,18 @@ data Camera = Camera { focalLength :: Double
 
 
 defaultCamera :: Camera
-defaultCamera = Camera f w h rs
+defaultCamera = mkCamera 800 600 2.0 (mkPoint [ 0, 0, 0 ])
+
+mkCamera :: Int -> Int -> Double -> Point -> Camera
+mkCamera w h f o = Camera f w h rs
   where
-    w = 800
-    h = 600
-    f = 2.0
-    o = mkPoint [ 0, 0, 0 ]
+    aR = (fromIntegral w) / (fromIntegral h) -- assumes square pixels
     rs = map mkRay [ (fromIntegral x, fromIntegral y)
                    | y <- [(h - 1), (h - 2)..0]
                    , x <- [(w - 1), (w - 2)..0]
                    ]
-    mkRay (x,y) = Ray o . normalize $ V.fromList [ 1.0, (x / fromIntegral w) - 0.5,(y / fromIntegral h) - 0.5]
+    mkRay (x,y) = Ray o . normalize $
+                  V.fromList [ 1.0
+                             , ((x / fromIntegral w) - 0.5) -- scaled to [ -0.5,0.5 ]
+                             , ((y / fromIntegral h) - 0.5) / aR -- scaled and aspect adjusted
+                             ]
